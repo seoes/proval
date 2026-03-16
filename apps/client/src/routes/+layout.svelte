f0442b00603734fbd52458567a5efe7e68eefd47
dowonseo <dsa3182@gmail.com>
2026-03-16 13:15:01 +0900

    Set layout, add sidebar and modal root on layout

--- apps/client/src/routes/+layout.svelte
+++ apps/client/src/routes/+layout.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+    import './layout.css';
+    import favicon from '$lib/assets/favicon.svg';
+    import Sidebar from '$lib/components/Sidebar.svelte';
+    import ModalRoot from '$lib/components/organism/ModalRoot.svelte';
 
-	let { children } = $props();
+    let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<div class="flex">
+    <aside class=" w-84 border-r border-neutral-300">
+        <Sidebar />
+    </aside>
+    <div class="w-full bg-neutral-50 px-4 pt-8">
+        <header></header>
+        <main class="">
+            {@render children?.()}
+        </main>
+        <footer></footer>
+    </div>
+</div>
+<ModalRoot />
