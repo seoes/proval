02c4a684ed1572e20b183dafd6346b277edd338d
dowonseo <dsa3182@gmail.com>
2026-05-27 10:50:51 +0900

    refactor: remove repository middleware

--- apps/api/src/webhook/webhook.route.ts
+++ apps/api/src/webhook/webhook.route.ts
@@ -6,10 +6,9 @@ import { handleGitHubWebhook } from "./github/github.controller.js";
 import { loadForgejoContext } from "./forgejo/forgejo.middleware.js";
 import { handleForgejoWebhook } from "./forgejo/forgejo.controller.js";
 import { logWebhookIngress } from "./webhook.middleware.js";
-import { updateRepositoryPath } from "./repository.middleware.js";
 
 export const webhookRouter = new Hono();
 
-webhookRouter.post("/gitlab", loadGitLabContext, updateRepositoryPath, logWebhookIngress, handleGitLabWebhook);
-webhookRouter.post("/github", loadGitHubContext, updateRepositoryPath, logWebhookIngress, handleGitHubWebhook);
-webhookRouter.post("/forgejo", loadForgejoContext, updateRepositoryPath, logWebhookIngress, handleForgejoWebhook);
+webhookRouter.post("/gitlab", loadGitLabContext, logWebhookIngress, handleGitLabWebhook);
+webhookRouter.post("/github", loadGitHubContext, logWebhookIngress, handleGitHubWebhook);
+webhookRouter.post("/forgejo", loadForgejoContext, logWebhookIngress, handleForgejoWebhook);
