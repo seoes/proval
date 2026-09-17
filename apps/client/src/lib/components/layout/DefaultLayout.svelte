cec04ee31d0aefda38c7523c917c5feeb5a461f8
dowonseo <dsa3182@gmail.com>
2026-09-17 11:42:43 +0900

    Set [DEV] suffix on page title in development mode

--- apps/client/src/lib/components/layout/DefaultLayout.svelte
+++ apps/client/src/lib/components/layout/DefaultLayout.svelte
@@ -11,7 +11,7 @@
 </script>
 
 <svelte:head>
-    <title>{title} — Proval</title>
+    <title>{title} — Proval{import.meta.env.DEV ? " [DEV]" : ""}</title>
 </svelte:head>
 
 <div class="mx-auto {narrow ? 'max-w-lg' : 'max-w-6xl'}">
